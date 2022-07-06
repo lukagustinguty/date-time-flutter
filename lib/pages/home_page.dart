034ea6bbfb781fa3d5332fd913c2d3c444a47b48
Luciano Gutierrez <lucianogutierrezagustin@gmail.com>
2022-07-06 09:39:23 -0300

    como poner a my page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,14 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    locale:
+    const Locale("ar", "AR");
+    DateTime now = DateTime.now();
+    String day = DateFormat('EEEE, MMM d, yyyy').format(now);
     return Scaffold(
       appBar: AppBar(
         title: Center(child: Text("DATE TIME")),
       ),
+      body: Center(child: Text(day)),
     );
   }
 }
