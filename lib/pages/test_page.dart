140da94421daa643e0780edfdb95d1db1ba0971c
Luk Gutierrez <lukgutierrezagustin@gmail.com>
2022-07-07 13:55:07 -0300

    List

--- lib/pages/test_page.dart
+++ lib/pages/test_page.dart
@@ -1,14 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
 
 class TestPage extends StatelessWidget {
   const TestPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    //Muy Importate de las comillas simples y dobles
     DateTime now = DateTime.now();
-    String texto = DateFormat(DateFormat.YEAR_MONTH_DAY, 'es_ES').format(now);
+    String texto =
+        DateFormat("d MMMM yyyy 'a' HH:mm 'hs.'", 'es_ES').format(now);
 
     return Scaffold(
       appBar: AppBar(),
@@ -17,7 +18,6 @@ class TestPage extends StatelessWidget {
           ListTile(
             title: Text(
               texto,
-              locale: const Locale("es", "ES"),
             ),
           ),
           Text(texto)
